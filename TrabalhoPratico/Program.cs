015d20f4f8720b495f43a5fc5e2457dfcc20f408
barbaragiovana03 <barbaragiovana1995@gmail.com>
2023-06-19 13:00:14 -0300

    1.0

---
+++ TrabalhoPratico/Program.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CampoMinado
+{
+    /*INTEGRANTES do GRUPO: Ana Paula Cardoso, Barbara Giovana, Hayanne Santos, Jader Santos.
+    Data da Criação: 15/06/2023      Última alteração: 
+    Objetivo do Programa: */
+
+    private int[,] tabuleiro;
+    private bool[,] tabuleiroRevelado;
+    private int linhas;
+    private int colunas;
+    private int totalBombas;
+
+    static void Main(string[] args)
+    {        
+        // Criação do objeto CampoMinado e chamada do método IniciarJogo()
+        CampoMinado campoMinado = new CampoMinado();
+        campoMinado.IniciarJogo();
+    }
+
+    // Método para iniciar o jogo
+    public void IniciarJogo()
+    {
+        //chamada do metodo para ler arquivo texto
+        lerArquivoTexto();
+
+        //inicializando tabuleiro com linhas e colunas em branco
+        tabuleiro = new int[linhas, colunas];
+        tabuleiroRevelado = new bool[linhas, colunas];
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                tabuleiro[i, j] = 0;
+                tabuleiroRevelado[i, j] = false;
+            }
+        }
+
+        //chamada do metodo para preecnher os numeros de bombas adjacentes
+        PreencherNumeros();
+
+        //chamada do metodo para posicionar as bombas
+        PreencherBombas();            
+      
+
+        while (true)
+        {
+            ImprimirTabuleiro();
+            Console.WriteLine("Digite a linha: ");
+            int linha = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a coluna: ");
+            int coluna = int.Parse(Console.ReadLine());
+
+
+            if (tabuleiroRevelado[linha, coluna])
+            {
+                Console.WriteLine("Essa célula já foi descoberta. Tente novamente.");
+                continue;
+            }
+
+            if (VerificarDerrota())
+            {
+                Console.WriteLine("Você perdeu! Acertou uma bomba.");
+                break;
+            }
+           
+            RevelarPosicao(linha, coluna);
+
+            if (VerificarVitoria())
+            {
+                Console.WriteLine("Parabéns! Você venceu!");
+                break;
+            }
+        }
+    }
+
+   
+
+    private void lerArquivoTexto()
+    {
+        string filePath = @"C:\TrabalhoPratico\arquivo.txt";
+
+      
+
+
+    }
+
+
+
+    // Método para preencher o tabuleiro com bombas aleatórias
+    private void PreencherBombas()
+    {
+        Random rd = new Random();
+
+        for (int i = 0; i < totalBombas; i++)
+        {
+            int linha;
+            int coluna;
+
+            do
+            {
+                linha = rd.Next(linhas);
+                coluna = rd.Next(colunas);
+            } while (tabuleiro[linha, coluna] == '*');
+
+            tabuleiro[linha, coluna] = '*';
+        }
+    }
+
+    // Método para mostrar o tabuleiro na tela
+    private void ImprimirTabuleiro()
+    {
+        Console.WriteLine("Tabuleiro:");
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                if (tabuleiroRevelado[i, j])
+                {
+                    Console.Write(tabuleiro[i, j]);
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    // Método para preencher os números ao redor das bombas
+    private void PreencherNumeros()
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                if (tabuleiro[i, j] == '*')
+                {
+                    continue;
+                }
+
+                int count = 0;
+
+                for (int x = i - 1; x <= i + 1; x++)
+                {
+                    for (int y = j - 1; y <= j + 1; y++)
+                    {
+                        if (x >= 0 && x < linhas && y >= 0 && y < colunas && tabuleiro[x, y] == '*')
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                tabuleiro[i, j] = count;
+            }
+        }
+    }
+
+
+    private void RevelarPosicao(int linha, int coluna)
+    {
+        tabuleiroRevelado[linha, coluna] = true;
+
+        if (tabuleiro[linha, coluna] == 0)
+        {
+            DescobrirCelulasAdjacentes(linha, coluna);
+        }
+    }
+
+    private void DescobrirCelulasAdjacentes(int linha, int coluna)
+    {
+        for (int i = linha - 1; i <= linha + 1; i++)
+        {
+            for (int j = coluna - 1; j <= coluna + 1; j++)
+            {
+                if (i >= 0 && i < linhas && j >= 0 && j < colunas && !tabuleiroRevelado[i, j])
+                {
+                    RevelarPosicao(i, j);
+                }
+            }
+        }
+    }
+
+    // Método para verificar se o jogador ganhou o jogo
+    private bool VerificarVitoria()
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                if (!tabuleiroRevelado[i, j] && tabuleiro[i, j] != '*')
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // Método para verificar se o jogador perdeu o jogo
+    private bool VerificarDerrota()
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                if (tabuleiroRevelado[i, j] && tabuleiro[i, j] == '*')
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
+
+
+
+
+
+
+
---
+++ exemplo/Program.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CampoMinado
+{
+    /*INTEGRANTES do GRUPO: Ana Paula Cardoso, Barbara Giovana, Hayanne Santos.
+    Data da Criação: 15/06/2023      Última alteração:*/
+
+    private int[,] tabuleiro;
+    private bool[,] tabuleiroRevelado;
+    private int linhas;
+    private int colunas;
+    private int totalBombas;
+    private string nome;
+
+    static void Main(string[] args)
+    {
+        // Criação do objeto CampoMinado e chamada do método IniciarJogo()
+        CampoMinado campoMinado = new CampoMinado();
+        Console.WriteLine("Bem-Vindo ao CAMPO MINADO! \nPor favor informe seu Nome:");
+        campoMinado.nome = Console.ReadLine();
+
+       // Console.WriteLine($"Muito bem {campoMinado.nome} vamos iniciar!\nObjetivo do JOGO: revele todas as posições sem selecionar uma mina. Boa sorte:");
+        
+
+        campoMinado.IniciarJogo();
+    }
+
+    // Método para iniciar o jogo
+    public void IniciarJogo()
+    {
+        //Chamada do metodo para ler arquivo texto
+        lerArquivoTexto();
+
+        //Inicializando tabuleiro de linhas e colunas com 0
+        tabuleiro = new int[linhas, colunas];
+        tabuleiroRevelado = new bool[linhas, colunas];
+
+         for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                tabuleiro[i, j] = 0;
+                tabuleiroRevelado[i, j] = false;
+            }
+        }
+
+        //Chamada do metodo para posicionar as bombas
+        PreencherBombas();
+        //Chamada do metodo para preencher o numero de bombas adjacentes
+        PreencherNumeros();
+
+       
+        
+        while (true)
+        {
+            Console.WriteLine();
+            ImprimirTabuleiro();
+            Console.Write("\nDigite a linha: ");
+            int linha = int.Parse(Console.ReadLine());
+            Console.Write("Digite a coluna: ");
+            int coluna = int.Parse(Console.ReadLine());
+
+            RevelarPosicao(linha, coluna);       
+
+            if (VerificarVitoria())
+            {
+                Console.WriteLine("Parabéns! Você venceu!");
+                break;
+            }
+           if (VerificarDerrota())
+            {
+                Console.WriteLine("Você perdeu! Acertou uma bomba.");
+                break;
+            }
+        }
+    }
+
+    private void lerArquivoTexto()
+    {               
+        this.linhas = 8;
+        this.colunas = 8;
+        this.totalBombas = 8;
+    }
+
+    // Método para preencher o tabuleiro com bombas aleatórias
+    private void PreencherBombas()
+    {
+        Random rd = new Random();
+        for (int i = 0; i < totalBombas; i++)
+        {
+            int linha;
+            int coluna;
+            do
+            {
+                linha = rd.Next(linhas);
+                coluna = rd.Next(colunas);
+            } while (tabuleiro[linha, coluna] == '*');
+
+            tabuleiro[linha, coluna] = '*';
+        }
+    }
+
+    // Método para preencher os números ao redor das bombas
+    private void PreencherNumeros()
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                if (tabuleiro[i, j] == '*')
+                {
+                    continue;
+                }
+
+                int count = 0;
+                for (int x = i - 1; x <= i + 1; x++)
+                {
+                    for (int y = j - 1; y <= j + 1; y++)
+                    {
+                        if (x >= 0 && x < linhas && y >= 0 && y < colunas && tabuleiro[x, y] == '*')
+                        {
+                            count++;
+                        }
+                    }
+                }
+                tabuleiro[i, j] = count;
+            }
+        }
+    }
+
+    // Método para mostrar o tabuleiro na tela
+    private void ImprimirTabuleiro()
+    {     
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                if (tabuleiroRevelado[i, j])
+                {
+                    Console.Write(tabuleiro[i, j]);
+                }
+                else
+                {
+                    Console.Write("x");
+                }
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    } 
+
+    private void RevelarPosicao(int linha, int coluna)
+    {
+        if (tabuleiroRevelado[linha, coluna])
+        {
+            Console.WriteLine("\nEste campo já foi aberto! Escolha outra opção.");
+            return;
+        }
+
+        tabuleiroRevelado[linha, coluna] = true;
+
+        if (tabuleiro[linha, coluna] == 0)
+        {
+            DescobrirCelulasAdjacentes(linha, coluna);
+        }
+        else if (tabuleiro[linha, coluna] == '*')
+        {
+            VerificarDerrota();
+        }
+    }
+
+    private void DescobrirCelulasAdjacentes(int linha, int coluna)
+    {
+        // Verificar célula acima
+        if (linha - 1 >= 0 && !tabuleiroRevelado[linha - 1, coluna])
+        {
+            RevelarPosicao(linha - 1, coluna);
+        }
+        // Verificar célula abaixo
+        if (linha + 1 < linhas && !tabuleiroRevelado[linha + 1, coluna])
+        {
+            RevelarPosicao(linha + 1, coluna);
+        }
+        // Verificar célula à esquerda
+        if (coluna - 1 >= 0 && !tabuleiroRevelado[linha, coluna - 1])
+        {
+            RevelarPosicao(linha, coluna - 1);
+        }
+        // Verificar célula à direita
+        if (coluna + 1 < colunas && !tabuleiroRevelado[linha, coluna + 1])
+        {
+            RevelarPosicao(linha, coluna + 1);
+        }
+    }
+
+    // Método para verificar se o jogador ganhou o jogo
+    private bool VerificarVitoria()
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                if (!tabuleiroRevelado[i, j] && tabuleiro[i, j] != '*')
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Método para verificar se o jogador perdeu o jogo
+    private bool VerificarDerrota()
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                if (tabuleiroRevelado[i, j] && tabuleiro[i, j] == '*')
+                {
+                    return true;
+                }
+            }
+        }
+        return false;      
+    }
+}
+
+
+
+
+
+
+
